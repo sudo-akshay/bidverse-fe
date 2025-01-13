0671ccdc418f13b0504e27844af92709fe942ab5
sudo-akshay <akshaytap1996@gmail.com>
2025-01-13 10:54:26 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -4,6 +4,6 @@ export const POSITION_CATEGORIES = ["FORWARD", "MIDFIELDER", "DEFENDER", "GOALKE
 
 // export const API_BASE_URL = 'https://3239-122-172-83-180.ngrok-free.app/api';
 
-export const API_BASE_URL = 'https://fc0a-2401-4900-882c-a45c-d0ed-2ef0-991d-9a01.ngrok-free.app';
+export const API_BASE_URL = 'https://fc0a-2401-4900-882c-a45c-d0ed-2ef0-991d-9a01.ngrok-free.app/api';
 
 // export const API_BASE_URL = 'http://localhost:8080/api';
